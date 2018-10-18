3fadf1e9b357e3898d1260140e722e969b59c796
Anthony Garcia <garcia.anthony577@gmail.com>
2018-10-18 13:40:58 -0600

    beginning to validate uuid on php document

--- php/Classes/ValidateUuid.php
+++ php/Classes/ValidateUuid.php
@@ -6,24 +6,21 @@ require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
 use Ramsey\Uuid\Uuid;
 
 class Article {
+	/**
+	 *id for this article
+	 * @var Uuid $articleId
+	 **/
 	private $articleId;
+	/**
+	 * id for the Author that wrote this article; this is a foreign key
+	 * @var Uuid $articleAuthorId
+	 */
 	private $articleAuthorId;
+	/**
+	 *
+	 * @var
+	 */
 	private $articleContent;
 	private $articleTitle;
 	private $articleAlbumId;
-
-	/**
-	 * constructor for this Article
-	 *
-	 * @param string|Uuid $newArticleId id of this Article or null if a new Article
-	 * @param string|Uuid $newArticleAuthorId id of the Author that wrote this Article
-	 * @param string $newArticleContent string containing actual Article data
-	 * @param \DateTime|string|null $newTweetDate date and time Tweet was sent or null if set to current date and time
-	 * @throws \InvalidArgumentException if data types are not valid
-	 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
-	 * @throws \TypeError if data types violate type hints
-	 * @throws \Exception if some other exception occurs
-	 * @Documentation https://php.net/manual/en/language.oop5.decon.php
-	 **/
-
-} 
+}
